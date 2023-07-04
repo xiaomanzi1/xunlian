3c045092e259a19e88d592b3a3e9495d983090fa
xiaomanzi1 <2224976321@qq.com>
2023-07-04 16:12:23 +0800

    1

---
+++ p5690.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m,d,count=0;
+    int a[13]={
+        0,31,28,31,30,31,30,31,31,30,31,30,31
+    };
+    scanf("%d-%d",&m,&d);
+    return 0;
+}
